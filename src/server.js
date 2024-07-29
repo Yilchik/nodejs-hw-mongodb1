eaa1e1f11118e79b0397ac0cc575e6959fc94d59
Yilchik <yulyayulia2@gmail.com>
2024-07-29 14:45:05 +0200

    status

--- src/server.js
+++ src/server.js
@@ -47,6 +47,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -58,12 +59,14 @@ export const setupServer = () => {
 
     if (!contact) {
       res.status(404).json({
+        status: 404,
         message: 'Contact not found',
       });
       return;
     }
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contact with id ${contactId}!',
       data: contact,
     });
