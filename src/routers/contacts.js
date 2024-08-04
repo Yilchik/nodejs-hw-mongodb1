cee8b45418cb85485e63c89e94a17672fbdd96b0
Yilchik <yulyayulia2@gmail.com>
2024-08-04 16:34:01 +0200

    controller

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,35 +1,13 @@
 import { Router } from 'express';
-import { getAllContacts, getContactsById } from './services/contacts.js';
+import {
+  getContactsByIdController,
+  getContactsController,
+} from '../controllers/contacts';
 
 const router = Router();
 
-router.get('/contacts', async (req, res) => {
-  const contacts = await getAllContacts();
+router.get('/contacts', getContactsController);
 
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
-});
-
-router.get('/contacts/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await getContactsById(contactId);
-
-  if (!contact) {
-    res.status(404).json({
-      status: 404,
-      message: 'Contact not found',
-    });
-    return;
-  }
-
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contact with id ${contactId}!',
-    data: contact,
-  });
-});
+router.get('/contacts/:contactId', getContactsByIdController);
 
 export default router;
