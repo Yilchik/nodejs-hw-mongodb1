cc284cebc589429e1f84c834a846f9209aecf793
Yilchik <yulyayulia2@gmail.com>
2024-08-09 14:01:57 +0200

    isValidId

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,7 +8,10 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema } from '../validation/contacts.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
@@ -22,7 +25,11 @@ router.post(
   ctrlWrapper(createContactController),
 );
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
