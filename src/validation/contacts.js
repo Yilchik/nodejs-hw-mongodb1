61b2530cbc384d93dca4953e7a68a986ffee436a
Yilchik <yulyayulia2@gmail.com>
2024-08-09 13:56:46 +0200

    createContactSchema

---
+++ src/validation/contacts.js
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  email: Joi.string().min(3).max(20).email().required(),
+  age: Joi.number().integer().min(6).max(16).required(),
+  gender: Joi.string().valid('male', 'female', 'other').required(),
+  avgMark: Joi.number().min(2).max(12).required(),
+  onDuty: Joi.boolean(),
+});
