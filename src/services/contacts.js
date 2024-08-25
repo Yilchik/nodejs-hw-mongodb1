4b6de8db62767c05905940d65c120e135abafa6e
Yilchik <yulyayulia2@gmail.com>
2024-08-25 13:11:44 +0200

    uderId

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -12,10 +12,14 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.findOne({ _id: contactId, userId });
+  const contactsQuery = ContactsCollection.find();
+
   if (filter.isFavourite) {
     contactsQuery.where('contactsQuery').equals(filter.isFavourite);
   }
+
+  contactsQuery.where('userId').equals(userId);
+
   const contactsCount = await ContactsCollection.find().countDocuments(
     contactsQuery,
   );
