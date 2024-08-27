0268aab1bf843211bdeeeb0e013b88b923fd37f3
Yilchik <yulyayulia2@gmail.com>
2024-08-27 08:04:13 +0200

    path

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -48,5 +48,5 @@ export const updateContactSchema = Joi.object({
   contactType: Joi.string().valid('work', 'home', 'personal').messages({
     'string.base': 'User type should be a string',
   }),
-  userId: Joi.string().required(),
+  userId: Joi.string(),
 });
