bfcc5079456591986e760f4a42ae3dcee13257d3
Yilchik <yulyayulia2@gmail.com>
2024-08-28 19:04:51 +0200

    SMTP

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,11 @@ export const SORT_ORDER = {
 };
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const DAYS = 30 * 24 * 60 * 60 * 1000;
+
+export const SMTP = {
+  SERVER: process.env.SMTP_SERVER,
+  PORT: process.env.SMTP_PORT,
+  USER: process.env.SMTP_USER,
+  PASSWORD: process.env.SMTP_PASSWORD,
+  FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
+};
