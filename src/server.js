404fac45b9ba2fdf8106a4c43a3445cd57c9acca
Yilchik <yulyayulia2@gmail.com>
2024-09-01 22:02:44 +0200

    get all

--- src/server.js
+++ src/server.js
@@ -8,12 +8,14 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
 
+  app.use('/api-docs', swaggerDocs());
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
